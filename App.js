c8c8a241aed98b4222b275eb9f3f4e78c92cb4cd
Try <khovct@gmail.com>
2019-08-24 11:52:43 -0700

    added firebase configuration

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, FlatList } from 'react-native';
+import { Input, Card, Button, Icon } from 'native-base';
+import * as firebase from firebase;
+
+// firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyC1AC08ZnST0_R3ToAxGBZ0b8Ma-clcLds",
+  authDomain: "reactbootcamp-fab67.firebaseapp.com",
+  databaseURL: "https://reactbootcamp-fab67.firebaseio.com",
+  projectId: "reactbootcamp-fab67",
+  storageBucket: "",
+  messagingSenderId: "897342952156",
+  appId: "1:897342952156:web:bc63d2be62e7e10c"
+};
+
 
 export default function App() {
   return (

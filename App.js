a7a6d19f6be712fc2b6c50bc06794cbb6905e9bc
Try <khovct@gmail.com>
2019-08-24 12:09:49 -0700

    added the css elements and added new functions for messages

--- App.js
+++ App.js
@@ -15,19 +15,89 @@ let firebaseConfig = {
 };
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      messageList: []
+    }
+  };
+
+  sendMessage = message => {
+    let messageListRef = firebase.database().ref("message_list");
+    let newMessageRef = messageListRef.push(); // pushes message to database
+    newMessageRef.set({
+      text: message,
+      time: Date.now()
+    });
+    this.setState({message: ""}) // when message is sent, you want it to be cleared again
+  };
+
+  updateList = messageList => {
+    this.setState({messageList: messageList});
+  };
+
+  componentDidMount() {
+    let self = this;
+    let messageListRef = firebase.database().ref("message_list");
+    messageListRef.on("value", dataSnapShot => {
+      // into a callback
+      if(dataSnapShot.val()) {
+        let messageList = Object.values(dataSnapShot.val());
+        self.updateList(messageList); // since this is a callback, we can't use this
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    margin: 2,
+    backgroundColor: "#01CBC6"
   },
+  header: {
+    backgroundColor: "#2B2B52",
+    alignItems: "center",
+    height: 40,
+    justifyContent: "center"
+  },
+  headerText: {
+    paddingHorizontal: 10,
+    color: "#FFF",
+    fontSize: 20
+  },
+  listContainer: {
+    flex: 1,
+    padding: 5
+  },
+  listItem: {
+    padding: 10
+  },
+  messageText: {
+    fontSize: 20
+  },
+  timeText: {
+    fontSize: 10
+  },
+  inputContainer: {
+    flexDirection: "row",
+    padding: 5,
+    borderWidth: 5,
+    borderRadius: 15,
+    borderColor: "#2B2B52",
+    color: "#fff"
+  }
 });
+
